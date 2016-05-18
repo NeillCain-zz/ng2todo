6bb28e26803a599b4a362909634166987f56865d
Chopper-sledgehammer22 <cainbot22@gmail.com>
2016-05-19 00:39:21 +0100

    adds bs4 and ng2 bs with some layout tweaks

--- app/home/home.component.ts
+++ app/home/home.component.ts
@@ -1,13 +1,26 @@
 import {Component, OnInit} from '@angular/core';
 import {StateService} from '../common/state.service';
+import {AlertComponent} from 'ng2-bootstrap/ng2-bootstrap';
 
 @Component({
   selector: 'home',
-  template: require('./home.component.html')
+  template: `
+  <div class="page-header m-t-1">
+    <h1>{{ title }}</h1>
+  </div>
+  <hr>
+  <div>
+    <alert type="info">Message {{ message }} - Today: {{ date.toDateString() }}</alert>
+    <form class="form-inline">
+      <input type="text" [(ngModel)]="message" placeholder="Message">
+      <button type="submit" class="btn btn-primary-outline" (click)="updateMessage(message)">Update Message</button>
+    </form>
+  </div>`,
+  directives: [AlertComponent]
 })
 export class HomeComponent implements OnInit {
-  title: string = 'Home Page';
-  body:  string = 'This is the about home body';
+  title: string = 'Home';
+  date: Date = new Date();
   message: string;
 
   constructor(private _stateService: StateService) { }
