9e9796a2e1f8c0d69d48b761b3fbec4234bead0e
Chopper-sledgehammer22 <cainbot22@gmail.com>
2016-05-19 19:28:27 +0100

    take 10 todos + file formatting

--- app/common/todo.service.ts
+++ app/common/todo.service.ts
@@ -6,37 +6,38 @@ import { Headers, RequestOptions } from '@angular/http';
 
 @Injectable()
 export class TodoService {
-  constructor (private http: Http) {}
+  constructor(private http: Http) { }
   private todoUrl = 'http://todo.kungfoobar.me/todo';
-  getTodos (): Observable<Todo[]> {
+  getTodos(): Observable<Todo[]> {
     let headers = new Headers();
     headers.append('If-None-Match', '715347285830');
-    return this.http.get(this.todoUrl, {headers})
-                    .map(this.extractData)
-                    .catch(this.handleError);
+    return this.http.get(this.todoUrl, { headers })
+      .map(this.extractData)
+      .catch(this.handleError);
   }
 
-  addTodo (note: string, priority: number, status: string) : Observable<Todo> {
-    let body = JSON.stringify({ note, priority, status });
+  addTodo(note: string, priority: number, status: string): Observable<Todo> {
+    let todo = { note, priority, status };
+    let body = JSON.stringify(todo);
     let headers = new Headers({ 'Content-Type': 'application/json' });
     let options = new RequestOptions({ headers: headers });
 
     return this.http.post(this.todoUrl, body, options)
-                    .map(this.extractData)
-                    .catch(this.handleError);
+      .map(() => todo)
+      .catch(this.handleError);
   }
 
   private extractData(res: Response) {
-    return res.json().map(function(todo){
+    return res.json().slice(20, 30)
+      .map(function (todo) {
         todo.created = new Date(todo.created).toDateString();
         return todo;
-    })
-    .sort(function(a, b) {
-      return a.priority - b.priority;
-});
-
+      })
+      .sort(function (a, b) {
+        return a.priority - b.priority;
+      });
   }
-  private handleError (error: any) {
+  private handleError(error: any) {
     let errMsg = error.message || error.statusText || 'Server error';
     console.error(errMsg); // log to console instead
     return Observable.throw(errMsg);
