2b8d5f43fef187129a507e2e80280da0b5de2c77
Chopper-sledgehammer22 <cainbot22@gmail.com>
2016-05-19 19:28:54 +0100

    extracted add into separate component

---
+++ app/todo/addtodo.component.ts
@@ -0,0 +1,48 @@
+import {Component, OnInit} from '@angular/core';
+import {TodoService} from '../common/todo.service';
+import {AlertComponent} from 'ng2-bootstrap/ng2-bootstrap';
+import {Todo} from '../common/todo.model'
+
+@Component({
+    selector: 'addtodo',
+    template: `
+  Add another one??????
+  <input placeholder="NOTE" #note />
+  <input placeholder="PRIORITY" #priority />
+  <input placeholder="STATUS" #status />
+  <button (click)="addTodo(note.value, priority.value, status.value);">
+    Add Todo
+  </button>
+  <div *ngIf="errorMessage" class="alert alert-danger" role="alert">
+    <strong>{{errorMessage}}</strong>
+  </div>
+  <alert *ngFor="let alert of alerts;let i = index" [type]="alert.type" dismissible="true" (close)="closeAlert(i)">
+     {{ alert?.msg }}
+  </alert>
+  `,
+    directives: [AlertComponent],
+    providers: [TodoService]
+})
+
+export class AddTodoComponent {
+    constructor(private todoService: TodoService) { }
+    public alerts: Array<Object> = [];
+
+    public closeAlert(i: number): void {
+        this.alerts.splice(i, 1);
+    }
+
+    private addAlert(todo: Todo): void {
+        this.alerts.push({ msg: `Added ${todo.note}`, type: 'success', closable: true });
+    }
+    errorMessage: string;
+    showAlert: boolean;
+    newTodo: Todo;
+    addTodo(note: string, priority: number, status: string) {
+        if (!note && !priority && !status) { return; }
+        this.todoService.addTodo(note, priority, status)
+            .subscribe(
+            todo => this.addAlert(todo),
+            error => this.errorMessage = error);
+    }
+}
--- app/todo/todo.component.ts
+++ app/todo/todo.component.ts
@@ -1,8 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {TodoService} from '../common/todo.service';
-import {AlertComponent} from 'ng2-bootstrap/ng2-bootstrap';
 import {Todo} from '../common/todo.model'
 import { NgClass } from '@angular/common';
+import {AddTodoComponent} from './addtodo.component'
 
 @Component({
   selector: 'todo',
@@ -16,19 +16,12 @@ import { NgClass } from '@angular/common';
       <span>{{ todo.note}} </span>
       <span class="label label-default label-pill pull-xs-right">{{todo.created}} </span>
       <span class="label label-default label-pill pull-xs-right">{{todo.priority}}</span>
-
     </li>
   </ul>
-  New Todo:
-  <input placeholder="NOTE" #note />
-  <input placeholder="PRIORITY" #priority />
-  <input placeholder="STATUS" #status />
-  <button (click)="addTodo(note.value, priority.value, status.value);">
-    Add Todo
-  </button>
+  <addtodo></addtodo>
   <div class="error" *ngIf="errorMessage">{{errorMessage}}</div>`,
-  directives: [AlertComponent],
-  providers: [TodoService]
+  providers: [TodoService],
+  directives: [AddTodoComponent]
 })
 
 export class TodoComponent implements OnInit {
@@ -40,13 +33,6 @@ export class TodoComponent implements OnInit {
     this.todoService.getTodos()
       .subscribe(
       todos => this.todos = todos,
-      error => this.errorMessage = error);
-  }
-  addTodo(note: string, priority: number, status: string) {
-    if (!note && !priority && !status) { return; }
-    this.todoService.addTodo(note, priority, status)
-      .subscribe(
-      hero => this.todos.push(hero),
       error => this.errorMessage = error);
   }
 }
