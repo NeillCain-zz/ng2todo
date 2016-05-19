09cee45b3669b30035942ff76fc5af0e0007fa3a
Chopper-sledgehammer22 <cainbot22@gmail.com>
2016-05-19 17:33:23 +0100

    Http bindings or providers?

--- app/boot.ts
+++ app/boot.ts
@@ -3,10 +3,10 @@ import 'zone.js/dist/zone';
 
 import {bootstrap} from '@angular/platform-browser-dynamic';
 import {ROUTER_PROVIDERS} from '@angular/router';
-import {HTTP_BINDINGS} from '@angular/http';
+import {HTTP_PROVIDERS} from '@angular/http';
 import {AppComponent} from './app.component';
 
 bootstrap(AppComponent, [
   ROUTER_PROVIDERS,
-  HTTP_BINDINGS
+  HTTP_PROVIDERS
 ]);
