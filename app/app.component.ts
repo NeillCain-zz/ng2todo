3a8a768c5d2685457ad8fd05699af07082cd1396
Chopper-sledgehammer22 <cainbot22@gmail.com>
2016-05-20 11:07:46 +0100

    merge view and styles into app component coz jumoing between files sucks

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,12 +4,77 @@ import {AboutComponent} from './about/about.component';
 import {TodoComponent} from './todo/todo.component';
 import {HomeComponent} from './home/home.component';
 import {StateService} from './common/state.service';
-import {ExperimentsService} from './common/experiments.service';
 
 @Component({
   selector: 'app',
-  template: require('./app.component.html'),
-  styles: [require('./app.component.css')],
+  template: `
+<div class="pos-f-t">
+  <div class="collapse" id="navbar-header">
+    <div class="container bg-inverse p-a-1 color">
+      <h3>Collapsed content</h3>
+      <p>Toggleable via the navbar brand.</p>
+    </div>
+  </div>
+  <nav class="navbar navbar-light navbar-static-top bg-faded">
+    <div class="container">
+      <button class="navbar-toggler hidden-sm-up" type="button" data-toggle="collapse" data-target="#exCollapsingNavbar2">
+            &#9776;
+          </button>
+      <div class="collapse navbar-toggleable-xs" id="exCollapsingNavbar2">
+        <h1 id="logo">
+          <a class="navbar-brand" [routerLink]="['/home']"></a>
+        </h1>
+        <ul class="nav navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" [routerLink]="['/home']">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" [routerLink]="['/about']">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" [routerLink]="['/todo']">Todo</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</div>
+
+<div class="container foo">
+  <router-outlet></router-outlet>
+</div>
+
+<footer class="footer">
+  <div class="container">
+    <span class="text-muted">Place sticky footer content here.</span>
+  </div>
+</footer>
+  `,
+  styles: [`
+  h1#logo a {
+    background: url(../images/logo.png) no-repeat top left;
+    width: 194px;
+    height: 48px;
+    margin: 8px;
+    display: block;
+    text-indent: -9999px;
+  }
+
+  .nav-item .router-link-active {
+    color: #06c;
+  }
+  .footer {
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+    height: 40px;
+    background-color: #60c;
+  }
+
+  .foo {
+    padding-top: 70px;
+  }
+  `],
   directives: [ ROUTER_DIRECTIVES ],
   providers: [StateService],
 })
