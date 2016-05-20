7ace92bafa871bb37b6408f2dd527375cdacd147
Chopper-sledgehammer22 <cainbot22@gmail.com>
2016-05-20 18:28:58 +0100

    fucking annoying fixed footer interferes with ui

--- app/app.component.ts
+++ app/app.component.ts
@@ -40,7 +40,7 @@ import {StateService} from './common/state.service';
   <router-outlet></router-outlet>
 </div>
 
-<footer class="footer">
+<footer>
   <div class="container">
     <span class="text-muted">Place sticky footer content here.</span>
   </div>
