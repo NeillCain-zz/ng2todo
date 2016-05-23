94464dac79102cffd76dce6c7f8a538cd5e02d9d
Neill Cain <n.cain@craneware.com>
2016-05-23 17:50:36 +0100

    build prod with bits form ng2 examples

--- app/boot.ts
+++ app/boot.ts
@@ -1,8 +1,14 @@
 import {bootstrap} from '@angular/platform-browser-dynamic';
+import { enableProdMode } from '@angular/core';
 import {ROUTER_PROVIDERS} from '@angular/router';
 import {HTTP_PROVIDERS} from '@angular/http';
-import {AppComponent} from './app.component';
+
 import '../global_styles/testwebpack.css'
+import {AppComponent} from './app.component';
+
+if (process.env.ENV === 'production') {
+  enableProdMode();
+}
 
 bootstrap(AppComponent, [
   ROUTER_PROVIDERS,
