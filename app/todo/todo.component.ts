2694b237db47ae4eaefdad0b0e50affc984ce31f
Neill Cain <n.cain@craneware.com>
2016-05-24 18:02:43 +0100

    view child stuff, still weirdness with subscribe resolve firing multiple times on subsequent searches - seems to be search number + 2

--- app/todo/todo.component.ts
+++ app/todo/todo.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, Output, Input, EventEmitter} from '@angular/core';
+import {Component, OnInit, Output, Input, EventEmitter, ViewChild} from '@angular/core';
 import {Control, FORM_DIRECTIVES, NgClass} from '@angular/common';
 import {TodoService} from '../common/todo.service';
 import {Todo} from '../common/todo.model'
@@ -21,9 +21,11 @@ import {Observable, Observer} from 'rxjs/Rx';
 })
 
 export class TodoComponent implements OnInit {
+  @ViewChild(TodoSearchComponent) todoSearchComponent: TodoSearchComponent
   title: string
   errorMessage: string;
-  
+  private foobar: Observable<Todo[]>
+
   public todos :Todo[] = []
 
   constructor(private todoService: TodoService) {
@@ -32,13 +34,21 @@ export class TodoComponent implements OnInit {
   ngOnInit() {
     this.onSearch({skip: 0, take: 2});
     this.title = 'Todo List'
+    this.foobar = this.todoService.getTodos(0, 5)
+  }
+
+  ngAfterViewInit() {
+    this.todoSearchComponent.searchEvent.subscribe(foobar =>{
+      console.log('foobar', foobar);
+    });
   }
 
   onSearch(event) {
     this.todoService.getTodos(event.skip, event.take)
-	  .subscribe(result => {      
+	  .subscribe(result => {
+      console.log('result', result);
       this.todos = result;
-    }  
+    }
 	 , error => this.errorMessage = error);
 	}
 }
