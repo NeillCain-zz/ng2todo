b2c8c39287548021233348749165ea4e42f2f592
Neill Cain <n.cain@craneware.com>
2016-05-26 16:30:45 +0100

    clean up

--- app/todo/todo.component.ts
+++ app/todo/todo.component.ts
@@ -38,11 +38,9 @@ export class TodoComponent implements OnInit {
     this.title = 'Todo List'
     this.socket = sio.connect('ws://todo.kungfoobar.me');
     this.socket.on('post', data => {
-      console.log('post socket', data)
       this.toastie(data)
     })
     this.socket.on('put', data => {
-      console.log('put socket', data)
       this.toastie(data)
     })
   }
@@ -50,12 +48,9 @@ export class TodoComponent implements OnInit {
   ngAfterViewInit() {
     this.toastie({foo: 'initialised'});
     this.todoSearchComponent.searchEvent
+      .startWith({take: 10, skip: 0 })
       .flatMap(searchEvent => this.todoService.getTodos(searchEvent.skip, searchEvent.take))
-      .subscribe(result => {
-        console.log('result', result);
-        this.todos = result;
-      }
-      , error => this.errorMessage = error);
+      .subscribe(result => { this.todos = result }, error => this.errorMessage = error)
   }
 
   private toastie(data){
@@ -65,23 +60,9 @@ export class TodoComponent implements OnInit {
             title: 'Something happended on todo service',
             msg: message,
             showClose: true,
-            timeout: 5000,
-            onAdd: (toast: ToastData) => {
-                console.log('Toast ' + toast.id + ' has been added!');
-            },
-            onRemove: function(toast: ToastData) {
-                console.log('Toast ' + toast.id + ' has been removed!');
-            }
+            timeout: 3000
         };
 
         this.toastyService.default(toastOptions)
-        // switch (this.options.type) {
-        //     case 'default': this.toastyService.default(toastOptions); break;
-        //     case 'info': this.toastyService.info(toastOptions); break;
-        //     case 'success': this.toastyService.success(toastOptions); break;
-        //     case 'wait': this.toastyService.wait(toastOptions); break;
-        //     case 'error': this.toastyService.error(toastOptions); break;
-        //     case 'warning': this.toastyService.warning(toastOptions); break;
-        // }
   }
 }
