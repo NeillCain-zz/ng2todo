693b1beb2b3ff1a49a400bb7beb76e4fa5c43284
Chopper-sledgehammer22 <cainbot22@gmail.com>
2016-06-06 13:35:49 +0100

    attempts to get tests to run...

--- app/todo/todo.component.spec.ts
+++ app/todo/todo.component.spec.ts
@@ -11,12 +11,4 @@ xdescribe('Todo Component', () => {
   it('should be named `HomeComponent`', () => {
     expect(TodoComponent['name']).toBe('TodoComponent');
   });
-
-  xit('should have a method called `updateMessage`', () => {
-    expect(TodoComponent.prototype.getTodos).toBeDefined();
-  });
-
-  xit('should have a title Todo List', () => {
-    expect(TodoComponent.prototype.title).toBe('Todo List');
-  });
 });
--- webpack_config/webpack.test.js
+++ webpack_config/webpack.test.js
@@ -1,32 +1,33 @@
 var helpers = require('./helpers');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
     devtool: 'inline-source-map',
 
     resolve: {
         extensions: ['', '.ts', '.js'],
-        root: helpers.root('src'),
+        root: helpers.root('app'),
     },
 
     module: {
-        loaders: [
-            {
-                test: /\.ts$/,
-                loader: 'awesome-typescript-loader',
-            },
-            {
-                test: /\.html$/,
-                loader: 'html'
-
-            },
-            {
+        loaders: [{
+            test: /\.ts$/,
+            loader: 'awesome-typescript-loader',
+            exclude: [/\.e2e\.ts$/]
+        }, {
                 test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
-                loader: 'null'
-            },
-            {
+                loader: 'file?name=images/[name].[ext]'
+            }, {
                 test: /\.css$/,
-                loader: 'null'
-            }
-        ]
-    }
+                exclude: helpers.root('app'),
+                loader: ExtractTextPlugin.extract('style', 'css?sourceMap')
+            }, {
+                test: /\.(html|css)$/,
+                include: helpers.root('global_styles', 'app'),
+                loader: 'raw-loader'
+            }]
+    },
+    plugins: [
+        new ExtractTextPlugin('[name].css')
+    ]
 }
